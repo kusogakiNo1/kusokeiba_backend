fbdb8a9f891175e48b0c82db7b5fee74930aa30e
kusogaki.mp4 <kusogakimp4@gmail.com>
2025-07-05 21:36:14 +0900

    ユーザー登録APIの方も、バリデーションメッセージを定数化

--- src/constants/ValidationMessages.ts
+++ src/constants/ValidationMessages.ts
@@ -4,4 +4,25 @@ export const ValidationMsg = {
     unspecified: "idが未指定です。idを指定してください。",
     notInt: "idは数字で指定してください",
   },
+  userName: {
+    unspecified: "ユーザー名が入力されていません",
+    invalidLength: "ユーザー名は1文字以上20文字以下で入力してください",
+  },
+  email: {
+    unspecified: "メールアドレスが入力されていません",
+    invalidLength: "メールアドレスは255文字以下のものにしてください",
+    invalidFormat: "メールアドレスの形式が間違っています",
+    notUnique: "このメールアドレスはすでに使われています。",
+  },
+  password: {
+    unspecified: "パスワードが入力されていません",
+    invalidLength: "パスワードは8文字以上255文字以下にしてください",
+    invalidFormat:
+      "パスワードは数字と半角英字の両方を用いるようにしてください。また、それ以外の記号は用いないでください",
+  },
+  age: {
+    unspecified: "年齢が入力されていません",
+    notInt: "年齢が数字で入力してください",
+    invalidFormat: "年齢が負の数になっています",
+  },
 };
